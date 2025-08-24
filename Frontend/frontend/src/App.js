dd02854b88f994ab4b735ae4f1ad5a7bc752947c
Q-ARAYA <quiriatoa@gmail.com>
2025-08-23 20:49:42 -0600

    creacion del proyecto

---
+++ Frontend/frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from "react";
+import VentanaPrincipal from "./Interfaz/ventanas";
+
+function App() {
+  return <VentanaPrincipal />;
+}
+
+export default App;
